4dfe903d1cb2a8cc31157efd80144ab2158d0298
Edward Skrod <edskrod@microsoft.com>
2020-01-26 19:05:40 -0500

    Initial commit to repo.

---
+++ MyWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Windows;
+using OSVersionHelper;
+
+using MyWPFApp.Telemetry;
+
+namespace MyWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            packageName.Text = ThisAppInfo.GetDisplayName();
+            assemblyVersion.Text = ThisAppInfo.GetThisAssemblyVersion();
+            packageVersion.Text = ThisAppInfo.GetPackageVersion();
+            installedFrom.Text = ThisAppInfo.GetAppInstallerUri();
+            installLocation.Text = ThisAppInfo.GetInstallLocation();
+            DiagnosticsClient.TrackPageView(nameof(MainWindow));
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DiagnosticsClient.TrackEvent("ClickShowRuntimeInfo");
+
+            if (ButtonShowRuntimeVersionInfo.Content.ToString().StartsWith("Show"))
+            {
+                RuntimeVersionInfo.Text = ThisAppInfo.GetDotNetRuntimeInfo();                
+                ButtonShowRuntimeVersionInfo.Content = "Hide Runtime Info";
+            }
+            else
+            {
+                RuntimeVersionInfo.Text = "";
+                ButtonShowRuntimeVersionInfo.Content = "Show Runtime Info";
+            }
+        }
+    }
+}
---
+++ MyWpfApp.Tests/ThisAppInfoTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using MyWPFApp;
+
+namespace MyWPFApp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var thisAppInfo = new ThisAppInfo();
+        }
+
+        [Test]
+        public void GetDisplayName()
+        {
+            var displayName = ThisAppInfo.GetDisplayName();
+            Assert.AreEqual("Not packaged", displayName);
+        }
+
+        [Test]
+        public void GetAppInstallerUri()
+        {
+            var appInstallerUri = ThisAppInfo.GetAppInstallerUri();
+
+            Assert.AreEqual("Not packaged", appInstallerUri);
+        }
+    }
+}
